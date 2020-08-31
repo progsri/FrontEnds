0adfd71644eaa274f88e81389b5db00a4d0a8ee7
srikanth <machine.progsri@gmail.com>
2020-08-31 16:36:53 -0500

    adding UI components

--- Angular/udemy-maximillian-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/udemy-maximillian-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Recipe} from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [new Recipe("Palak paneer", "milk cubes mixed with green stuff", "https://static.toiimg.com/photo/53093667.cms")]
 
   constructor() { }
 
